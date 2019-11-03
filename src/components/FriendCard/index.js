2822bd70cb98e251765f05ea4209381aca0ad57e
Raul Gonzalez <rgonz166@gmail.com>
2019-11-02 20:49:34 -0700

    Add: card data to FriendCard index.js

---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,12 @@
+import React  from "react";
+import "./style.css";
+
+function FriendCard(props) {
+    return (
+        <div className="card">
+            {console.log("Card loaded")}
+        </div>
+    )
+}
+
+export default FriendCard;
